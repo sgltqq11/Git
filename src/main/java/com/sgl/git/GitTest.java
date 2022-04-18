eef8e8067502496448e7d811140ff03b00d92895
sgl <sgl@sgl.com>
2022-04-18 16:30:01 +0800

    second commit

--- src/main/java/com/sgl/git/GitTest.java
+++ src/main/java/com/sgl/git/GitTest.java
@@ -1,10 +1,14 @@
 package com.sgl.git;
 
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 public class GitTest {
     public static void main(String[] args) {
         ZonedDateTime now = ZonedDateTime.now();
         System.out.println(now);
+
+        Date date = new Date();
+        System.out.println(date);
     }
 }
