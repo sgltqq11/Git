fbc65f7968ca993209b3efb1b22e33f6e15746cc
sgl <sgl@sgl.com>
2022-04-18 16:25:22 +0800

    first commit

---
+++ src/main/java/com/sgl/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.sgl.git;
+
+import java.time.ZonedDateTime;
+
+public class GitTest {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
